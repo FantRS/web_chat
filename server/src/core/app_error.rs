496f20a0bd5c11db7dd8d33e8c5d2f62ab0540cb
FantRS <rostykkashper2@gmail.com>
2025-11-11 14:38:15 +0200

    improve error handling

--- server/src/core/app_error.rs
+++ server/src/core/app_error.rs
@@ -2,11 +2,11 @@ pub type AppResult<T> = Result<T, AppError>;
 
 #[derive(Debug, thiserror::Error)]
 pub enum AppError {
-    #[error("DatabaseError. Context: {0}")]
-    DatabaseError(String),
+    #[error("DBConnection. Context: {0}")]
+    SqlxError(#[from] sqlx::Error),
 
-    #[error("ParseError. Context: {0}")]
-    ParseError(String),
+    #[error("MigrateError. Context: {0}")]
+    MigrateError(#[from] sqlx::migrate::MigrateError),
 
     #[error("IoError. Context: {0}")]
     IoError(#[from] std::io::Error),
@@ -14,36 +14,15 @@ pub enum AppError {
     #[error("VarError. Context: {0}")]
     VarError(#[from] std::env::VarError),
 
-    #[error("BuildConfigError. Context: {0}")]
-    BuildConfigError(String),
-}
+    #[error("ParseBoolError. Context: {0}")]
+    ParseBoolError(#[from] std::str::ParseBoolError),
 
-impl From<sqlx::Error> for AppError {
-    fn from(value: sqlx::Error) -> Self {
-        Self::DatabaseError(value.to_string())
-    }
-}
+    #[error("ParseIntError. Context: {0}")]
+    ParseIntError(#[from] std::num::ParseIntError),
 
-impl From<sqlx::migrate::MigrateError> for AppError {
-    fn from(value: sqlx::migrate::MigrateError) -> Self {
-        Self::DatabaseError(value.to_string())
-    }
-}
+    #[error("ParseFloatError. Context: {0}")]
+    ParseFloatError(#[from] std::num::ParseFloatError),
 
-impl From<std::str::ParseBoolError> for AppError {
-    fn from(value: std::str::ParseBoolError) -> Self {
-        Self::ParseError(value.to_string())
-    }
-}
-
-impl From<std::num::ParseIntError> for AppError {
-    fn from(value: std::num::ParseIntError) -> Self {
-        Self::ParseError(value.to_string())
-    }
-}
-
-impl From<std::num::ParseFloatError> for AppError {
-    fn from(value: std::num::ParseFloatError) -> Self {
-        Self::ParseError(value.to_string())
-    }
+    #[error("OtherError. Context: {0}")]
+    Other(String),
 }
